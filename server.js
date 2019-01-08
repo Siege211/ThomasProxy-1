dfca86cf6d351d2a81dfc73d6fa2350b031fd7e6
JoonJang95 <joon.jang95@gmail.com>
2019-01-08 01:34:57 -0500

    Renders all four components with their data

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(/(\/\d+)/, express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
